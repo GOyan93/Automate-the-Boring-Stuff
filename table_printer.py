7b2f0d12ce3f9c7ed92046c4b00ac8ca215fe593
Greg <goyandle@gmail.com>
2020-05-14 14:36:22 -0400

    Added list of longest lengths per list

--- table_printer.py
+++ table_printer.py
@@ -5,4 +5,16 @@ Author: GOyan
 Date: May 14, 2020
 """
 
-tableData = [['apples', 'oranges', 'cherries', 'banana'], ['Alice', 'Bob', ' Carol', 'David'], ['dogs', 'cats', 'moose', 'goose']]
+table_data = [['apples', 'oranges', 'cherries', 'banana'], ['Alice', 'Bob', 'Carol', 'David'], ['dogs', 'cats', 'moose', 'goose']]
+
+def print_table(list_data):
+    length_list = []
+    for item in list_data:
+        longest_word = 0
+        for word in item:
+            if len(word) > longest_word:
+                longest_word = len(word)
+        length_list.append(longest_word)
+    print(length_list)
+
+print_table(table_data)
