dc5ad19d5a1a199ec244527938b674a43c837560
Greg <goyandle@gmail.com>
2020-05-14 15:09:16 -0400

    Added print function. End of project.

--- table_printer.py
+++ table_printer.py
@@ -9,12 +9,17 @@ table_data = [['apples', 'oranges', 'cherries', 'banana'], ['Alice', 'Bob', 'Car
 
 def print_table(list_data):
     length_list = []
+    reordered_data = []
     for item in list_data:
         longest_word = 0
         for word in item:
             if len(word) > longest_word:
                 longest_word = len(word)
         length_list.append(longest_word)
-    print(length_list)
-
+    for x in range(len(list_data[0])):
+	    for y in range(len(list_data)):
+		    print(list_data[y][x].rjust(length_list[y]), end = ' ')    
+	    print('')
+        
+    
 print_table(table_data)
