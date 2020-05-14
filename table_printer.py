83dd8a5f3b238b88fe6e80df3a9e72e3c20ebebc
Greg <goyandle@gmail.com>
2020-05-14 14:18:18 -0400

    Initial Commit

---
+++ table_printer.py
@@ -0,0 +1,8 @@
+"""
+Automate the Boring Stuff
+Title: Table Printer
+Author: GOyan
+Date: May 14, 2020
+"""
+
+tableData = [['apples', 'oranges', 'cherries', 'banana'], ['Alice', 'Bob', ' Carol', 'David'], ['dogs', 'cats', 'moose', 'goose']]
