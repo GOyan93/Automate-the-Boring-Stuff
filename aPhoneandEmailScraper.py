43b3abf18dd6c64b1d2389b4db313f8192dcb8d0
Greg <goyandle@gmail.com>
2020-05-14 21:37:21 -0400

    Completed copy function, organized number tuples and copied to clipboard. End of project.

--- aPhoneandEmailScraper.py
+++ aPhoneandEmailScraper.py
@@ -4,17 +4,18 @@ import re, pyperclip
 
 
 # Create a regex for phone numbers
-phone_regex = re.compile(r'''
+phone_regex = re.compile(r"""
 # 415-555-0000, 555-000, (415) 555-0000, 555-0000 ext 12345, ext. 12345, x12345
-
+(
 ((\d\d\d)|(\(d\d\d\)))?     # area code (optional)
 (\s|-)                      # first separator
 \d\d\d                      # first three digits
 -                           # separator
 \d\d\d\d                    # last for digits
 (((ext(\.)?/s)|x)           # extension word-part (optiona;)
-(/d{2,5})))?                # extension number part (optional)
-''', re.VERBOSE)
+(/d{2,5}))?
+)                           # extension number part (optional)
+""", re.VERBOSE)
 
 
 
@@ -33,5 +34,14 @@ text = pyperclip.paste()
                          
 # TODO: Extract the email/phone form this text
 extracted_phone = phone_regex.findall(text)
-extracted_email = email_regex.findall(text)                         
+extracted_email = email_regex.findall(text)
+
+all_phone_numbers = []
+for number in extracted_phone:
+    all_phone_numbers.append(number[0])
+    
+
+
 # TODO: Copy the extracted email/phone to the clipboard
+results = '\n'.join(all_phone_numbers) + '\n' + '\n'.join(extracted_email)
+pyperclip.copy(results)
